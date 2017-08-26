e556c70527409ab3f6f26bc7c2694d3218e3ec0e
Edward Isarevich <edward.isarevich@gmail.com>
2017-08-26 15:00:57 -0700

    events view work

--- client/app/events/events.component.js
+++ client/app/events/events.component.js
@@ -3,20 +3,51 @@ const angular = require('angular');
 
 const uiRouter = require('angular-ui-router');
 
-import routes from './events.routes';
+import routing from './events.routes';
 
-export class EventsComponent {
+export class EventsController {
+    awesomeThings = [];
+    newThing = '';
+
   /*@ngInject*/
-  constructor() {
+  constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.socket =  socket;
+      
+      $scope.$on('destroy', function(){
+          socket.unsyncUpdates('thing');
+      });
     this.message = 'Hello';
   }
+
+$onInit() {
+    this.$http.get('/api/things')
+      .then(response => {
+        this.awesomeThings = response.data;
+        this.socket.syncUpdates('thing', this.awesomeThings);
+      });
+  }
+
+  addThing() {
+    if(this.newThing) {
+      this.$http.post('/api/things', {
+        name: this.newThing
+      });
+      this.newThing = '';
+    }
+  }
+
+  deleteThing(thing) {
+    this.$http.delete(`/api/things/${thing._id}`);
+  }
+
 }
 
 export default angular.module('funnelTunnelApp.events', [uiRouter])
-  .config(routes)
+  .config(routing)
   .component('events', {
     template: require('./events.html'),
-    controller: EventsComponent,
+    controller: EventsController,
     controllerAs: 'eventsCtrl'
   })
   .name;
--- client/app/events/events.component.spec.js
+++ client/app/events/events.component.spec.js
@@ -1,17 +1,45 @@
 'use strict';
 
-describe('Component: EventsComponent', function() {
-  // load the controller's module
-  beforeEach(module('funnelTunnelApp.events'));
+import events from './events.component';
+import {
+  EventsController
+} from './events.component';
 
-  var EventsComponent;
+describe('Component: EventsController', function() {
+  // load the controller's module
+  //beforeEach(module('funnelTunnelApp.events'));
+  //beforeEach(module('stateMock'));
+  //beforeEach(module('socketMock'));
+
+  beforeEach(angular.mock.module(events));
+  beforeEach(angular.mock.module('stateMock'));
+  beforeEach(angular.mock.module('socketMock'));
+    
+  var eventsComponent;
+    var scope;
+    var state;
+    var $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function($componentController) {
-    EventsComponent = $componentController('events', {});
+  beforeEach(inject(function(_$httpBackend_, $http, $componentController, $rootScope, $state,
+    socket) {
+    $httpBackend = _$httpBackend_;
+    $httpBackend.expectGET('/api/things')
+      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
+
+    scope = $rootScope.$new();
+    state = $state;
+    eventsComponent = $componentController('events', {
+      $http,
+      $scope: scope,
+      socket
+    });
   }));
 
-  it('should ...', function() {
-    expect(1).to.equal(1);
+  it('should attach a list of things to the controller', function() {
+    mainComponent.$onInit();
+    $httpBackend.flush();
+    expect(mainComponent.awesomeThings.length)
+      .to.equal(4);
   });
 });
