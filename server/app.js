13a3223c781d722d546c05729fbd9573277ef135
Edward Isarevich <edward.isarevich@gmail.com>
2017-09-03 15:15:05 -0700

    updating app.js, removing import

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@
 
 //'use strict';
 
+/*
 
 import express from 'express';
 import mongoose from 'mongoose';
@@ -11,16 +12,16 @@ mongoose.Promise = require('bluebird');
 import config from './config/environment';
 import http from 'http';
 import seedDatabaseIfNeeded from './config/seed';
+*/
 
 
-/*
 var express = require('express');
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 var config = require('./config/environment');
 var http = require('http');
 var seedDatabaseIfNeeded = require('./config/seed');
-*/
+
 
 // Connect to MongoDB
 mongoose.connect(config.mongo.uri, config.mongo.options);
